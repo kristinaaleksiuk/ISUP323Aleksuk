0bec1853e671599158748044fc9842b8d4d9e9ab
Admin <kristinaalexiuk@yandex.ru>
2025-09-17 16:34:47 +0300

    Создание массива

--- pr1/Program.cs
+++ pr1/Program.cs
@@ -1,1 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Expense
+{
+    public string Name { get; set; }
+    public decimal Amount { get; set; }
+
+    public Expense(string name, decimal amount)
+    {
+        Name = name;
+        Amount = amount;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}; {Amount} руб.";
+    }
+}
