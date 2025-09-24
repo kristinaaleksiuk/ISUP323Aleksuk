f1adb343981a5dc0865697a1a9c7746088eb8da6
Admin <kristinaalexiuk@yandex.ru>
2025-09-24 11:31:41 +0300

    Программа принимает от пользователя минимум 100 символов

--- pr1/Program.cs
+++ pr1/Program.cs
@@ -1,1 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Пожалуйста, введите текст (минимум 100 символов):");
+        string userInput = Console.ReadLine();
+
+        if (string.IsNullOrEmpty(userInput) || userInput.Length < 100)
+        {
+            Console.WriteLine($"Ошибка! Вы ввели только {userInput?.Length ?? 0} символов. Нужно минимум 100 символов.");
+            return;
+        }
+        Console.WriteLine($"Отлично! Вы ввели {userInput.Length} символов.");
+        Console.WriteLine("Текст принят для дальнейшей обработки.");
+    }
+}
